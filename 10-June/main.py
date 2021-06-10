be533b85c315bb1ac2285d310ed8f2c58d30db47
Noufal Ibrahim <noufal@nibrahim.net.in>
2021-06-10 16:26:53 +0530

    Adds templatised flask app

--- 10-June/main.py
+++ 10-June/main.py
@@ -1,5 +1,6 @@
 import psycopg2
 from flask import Flask, request
+from flask import render_template
 
 
 app = Flask("Job site")
@@ -11,62 +12,18 @@ dbconn = psycopg2.connect("dbname=naukri")
 @app.route("/") 
 def index():
     cursor = dbconn.cursor()
-    cursor.execute("select count(*) from openings")
-    njobs = cursor.fetchall()[0][0]
-    
-    return f"""
-    <html>
-    <head>
-    <title> Jobs page </title>
-    </head>
-
-    <body>
-    <h1>Welcome to the jobs page</h1>
-    There are currently <a href="/jobs">{njobs}</a> jobs
-    </body>
-    </html>
-    """
+    cursor.execute("select count(*) from openings")   # Query
+    count_of_jobs = cursor.fetchall()[0][0]  # Get data
+    return render_template("main.html", njobs=count_of_jobs) # Render
 
 
 @app.route("/jobs") # decorator
 def jobs():
     cursor = dbconn.cursor()
-    cursor.execute("select title, company_name, jd_text from openings")
-    ret = []
+    cursor.execute("select title, company_name, jd_text from openings") # Query
+    jobs = cursor.fetchall() # Get data
+    return render_template("jobslist.html", jobs = jobs) #Render
 
 
-    for title, company_name, jd in cursor.fetchall():
-        item = f"<li>{title} :: {company_name} <br/> {jd}</li>"
-        ret.append(item)
-    jobs = "".join(ret)
-
-    return f"""
-    <html>
-    <head>
-    <title> Jobs page </title>
-    </head>
-
-    <body>
-    <h1>Welcome to the jobs page</h1>
-    <ol>
-    {jobs}
-    </ol>
-    </body>
-    </html>
-    """
-
-
-
-    return f"""List of jobs is:
-
-    {l}"""
-
-# http://127.0.0.1/add?item=x
-@app.route("/add")
-def add_item():
-    item = request.args.get("item")
-    items.append(item)
-    return f"No. of items is now {len(items)}"
-
-
-# https://flask.palletsprojects.com/en/2.0.x/
+if __name__ == "__main__":
+    app.run(debug=True)
